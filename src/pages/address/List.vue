117d0b023a662d18bdd4291bdd06b36e47cad9f9
wangyiwie <963542683@qq.com>
2020-01-04 09:51:01 +0800

    创完文件夹

---
+++ src/pages/address/List.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        address
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
---
+++ src/pages/employee/List.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        employee
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
