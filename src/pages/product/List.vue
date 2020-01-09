3b52d7c932c765e373fa61f8cf483fab789102f2
wangyiwie <963542683@qq.com>
2020-01-09 17:49:11 +0800

    修

--- src/pages/product/List.vue
+++ src/pages/product/List.vue
@@ -31,7 +31,7 @@
                 title="录入产品信息"
                 :visible.sync="visible"
                 width="60%">
-                {{form}}
+                <!-- {{form}} -->
                 <el-form ref="form" :model="form" label-width="80px">
                     <el-form-item label="产品名称">
                         <el-input v-model="form.name"></el-input>
