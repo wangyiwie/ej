4085c8b300850b1faf1f4e422730d5d2d3b05e22
crazy-robot55 <394368475@qq.com>
2020-01-09 10:40:07 +0800

    订单再次修改

--- src/pages/order/List.vue
+++ src/pages/order/List.vue
@@ -1,163 +1,121 @@
 <template>
   <div>
-    <el-tabs v-model="activeName" @tab-click="handleClick">
-    <el-tab-pane label="所有订单" name="first">
-      <el-table :data="orders.list">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客ID"></el-table-column>
-        <el-table-column prop="waiterId" label="员工ID"></el-table-column>
-        <el-table-column prop="addressId" label="地址ID"></el-table-column>
-        <el-table-column label="操作">
-          <template v-slot="slot">
-            <a href="" @click.prevent="">详情</a>
-          </template>
-        </el-table-column>
+    <!-- 选项卡 -->
+    <el-tabs v-model="params.status" @tab-click="loadData">
+      <el-tab-pane label="全部" name="全部"></el-tab-pane>
+      <el-tab-pane label="待派单" name="待派单"></el-tab-pane>
+      <el-tab-pane label="待接单" name="待接单"></el-tab-pane>
+      <el-tab-pane label="待服务" name="待服务"></el-tab-pane>
+      <el-tab-pane label="待确认" name="待确认"></el-tab-pane>
+      <el-tab-pane label="已完成" name="已完成"></el-tab-pane>
+    </el-tabs>
+    <!-- /选项卡 -->
+    <!-- 表格 -->
+    <el-table :data="orders.list">
+      <el-table-column prop="id" label="编号"></el-table-column>
+      <el-table-column width="200" prop="orderTime" label="下单时间"></el-table-column>
+      <el-table-column prop="total" label="总价"></el-table-column>
+      <el-table-column prop="status" label="状态"></el-table-column>
+      <el-table-column prop="customerId" label="顾客ID"></el-table-column>
+      <el-table-column prop="waiterId" label="员工ID"></el-table-column>
+      <el-table-column prop="addressId" label="地址ID"></el-table-column>
+      <el-table-column fixed="right" label="操作">
+        <template v-slot="slot">
+          <a href="javascript:void(0)" >详情</a>
+          <a href="" v-if="slot.row.status === '待派单'" @click.prevent="toSendOrderHandler(slot.row)">派单</a>
+          
+        </template>
+      </el-table-column>
     </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="待支付" name="second">
-      <el-table :data="orders.list">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客id"></el-table-column>
-      </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="待派单" name="third">
-      <el-table :data="orders.list">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客id"></el-table-column>
-        <el-table-column label="操作">
-          <template v-slot="slot">
-            <a href="" @click.prevent="sendOrder(slot.row.id)">派单</a>
-          </template>
-        </el-table-column>
-      </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="待接单" name="fourth">
-      <el-table :data="orders.list">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客id"></el-table-column>
-        <el-table-column label="操作"> 
-        </el-table-column>
-      </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="待服务" name="fifth">
-      <el-table :data="orders.list">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客id"></el-table-column>
-        <el-table-column prop="waiterId" label="员工id"></el-table-column>
-      </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="待确认" name="sixth">
-      <el-table :data="orders.list">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客id"></el-table-column>
-        <el-table-column prop="waiterId" label="员工id"></el-table-column>
-        </el-table>
-    </el-tab-pane>
-    <el-tab-pane label="已完成" name="seventh">
-      <el-table :data="orders.list">
-        <el-table-column prop="id" label="订单编号"></el-table-column>
-        <el-table-column prop="orderTime" label="下单时间"></el-table-column>
-        <el-table-column prop="total" label="总价"></el-table-column>
-        <el-table-column prop="status" label="状态"></el-table-column>
-        <el-table-column prop="customerId" label="顾客id"></el-table-column>
-        <el-table-column prop="waiterId" label="员工id"></el-table-column>
-      </el-table>
-    </el-tab-pane>
-  </el-tabs>
-    <!--分页开始 -->
-       <el-pagination 
+    <!-- /表格结束 -->
+    <!-- 分页开始 -->
+    <el-pagination 
+        :hide-on-single-page="true"
         layout="prev, pager, next" 
         :total="orders.total" 
         @current-change="pageChageHandler">
         </el-pagination>
-    <!--/分页结束 -->
+    <!-- /分页结束 -->
     <!-- 模态框 -->
     <el-dialog
-            title="派单"
-            :visible.sync="visible"
-            width="60%">
-            <template>
-                <el-radio-group v-model="radio">
-                    <el-radio :label="1">员工1</el-radio>
-                    <el-radio :label="2">员工2</el-radio>
-                    <el-radio :label="3">员工3</el-radio>
-                </el-radio-group>
-              </template>
-            <span slot="footer" class="dialog-footer">
-            <el-button @click="visible=false" size="small">取 消</el-button>
-            <el-button type="primary" @click="visible = false" size="small">确 定</el-button>
-            </span>
-      </el-dialog>
+      title="派单"
+      :visible.sync="visible"
+      width="60%">
+      <div>
+        <p> <strong>订单编号：</strong> {{form.id}} </p>
+        <p> <strong>订单总价：</strong> {{form.total}} </p>
+        <p> <strong>下单时间：</strong> {{form.orderTime}} </p>
+        <p> 
+          <strong>服务员工：</strong> 
+           <el-radio-group v-model="waiterId">
+            <el-radio 
+              border
+              size="small"
+              v-for="e in employees" 
+              :key="e.id"
+              :label="e.id" >{{e.realname}}</el-radio>
+          </el-radio-group>
+          
+        </p>
+      </div>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button size="small" @click="closeModalHandler">取 消</el-button>
+        <el-button size="small" type="primary" @click="submitHandler">确 定</el-button>
+      </span>
+    </el-dialog>
     <!-- /模态框 -->
 
   </div>
 </template>
+
 <script>
 import request from '@/utils/request'
 import querystring from 'querystring'
 export default {
   // 用于存放网页中需要调用的方法
   methods:{
-    sendOrder(){
-      this.visible = true;
-    },
-    //当分页中当前页改变时候执行
+    // 当分页中当前页改变的时候执行
     pageChageHandler(page){
-        //将params中的当前页改为插件中的当前页
+        // 将params中当前页改为插件中的当前页
         this.params.page = page-1;
-        //加载
+        // 加载
         this.loadData();
     },
+    // 加载员工信息
+    loadEmployees(){
+      let url = "http://localhost:6677/waiter/findAll";
+      request.get(url).then(response=>{
+        this.employees = response.data;
+      })
+    },
+    // 加载订单信息
     loadData(){
       let url = "http://localhost:6677/order/queryPage"
-    //   request.get(url).then((response)=>{
-    //     // 将查询结果设置到customers中，this指向外部函数的this
-    //     this.allorder = response.data;
-    //   })
-    request({
-      url,
-      method:"post",
-      headers:{
-        "Content-Type":"application/x-www-form-urlencoded"
-      },
-      data:querystring.stringify(this.params)
-    }).then((response)=>{
-      //orders为一个对象，其中包含了分页信息，以及列表信息
-      this.orders = response.data;
-    })
+      if(this.params.status === "全部"){
+        delete this.params.status;
+      }
+      request({
+          url,
+          method:"post",
+          headers:{
+              "Content-Type":"application/x-www-form-urlencoded"
+          },
+          data:querystring.stringify(this.params)
+      }).then((response)=>{
+          // orders为一个对象，其中包含了分页信息，以及列表信息
+          this.orders = response.data;
+      })
     },
     submitHandler(){
-      //this.form 对象 ---字符串--> 后台 {type:'customer',age:12}
-      // json字符串 '{"type":"customer","age":12}'
-      // request.post(url,this.form)
-      // 查询字符串 type=customer&age=12
-      // 通过request与后台进行交互，并且要携带参数
-      let url = "http://localhost:6677/order/saveOrUpdate";
+      let url = "http://localhost:6677/order/sendOrder";
       request({
         url,
-        method:"POST",
-        headers:{
-          "Content-Type":"application/x-www-form-urlencoded"
-        },
-        data:querystring.stringify(this.form)
+        method:"GET",
+        params:{
+          orderId:this.form.id,
+          waiterId:this.waiterId
+        }
       }).then((response)=>{
         // 模态框关闭
         this.closeModalHandler();
@@ -170,66 +128,38 @@ export default {
         })
       })
     },
-    toDeleteHandler(id){
-      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        //调用后台接口，完成删除操作
-        let url = "http://localhost:6677/order/deleteById?id"+id;
-        request.get(url).then((response)=>{
-          //刷新数据
-          this.loadData();
-          //提示结果
-          this.$message({
-          type: 'success',
-          message: response.message
-        });
-        })
-      })
-    },
-    toUpdateHandler(row){
-      //模态框表单中显示当前行信息
+    // 去派单，将模态框打开，然后选择员工作为派单对象
+    toSendOrderHandler(row){
+      // 模态框显示当前订单信息
       this.form = row;
       this.visible = true;
     },
     closeModalHandler(){
       this.visible = false;
-    },
-    toAddHandler(){
-      this.foem = {
-        type : "order"
-      }
-      this.visible = true;
     }
   },
   // 用于存放要向网页中显示的数据
   data(){
     return {
-      radio:'1',
-      radio:'2',
-      radio:'3',
       visible:false,
       orders:{},
-      
-      form:{
-        type:"orders"
-      },
-      
+      form:{},  // 当前订单
       params:{
-        page:0,
-        pageSize:10
-      }
+          page:0,
+          pageSize:10
+      },
+      employees:[],
+      waiterId:null // 选中的员工
     }
   },
   created(){
     // this为当前vue实例对象
     // vue实例创建完毕 
-    this.loadData()
-
+    this.loadData();
+    // 加载员工信息
+    this.loadEmployees();
   }
-} 
+}
 </script>
 
 <style scoped>
